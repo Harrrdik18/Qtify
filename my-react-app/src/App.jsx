13f94698ef8228f4903f3f784a4fc6010f6d26f0
hardik <harrrdik18@gmail.com>
2024-02-14 15:17:26 +0530

    updated logo

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -2,20 +2,18 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Navbar from './components/nav/navbar'
 import Button from './components/Button/Button'
 import Search from './components/Search/Search'
+
 
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
-  <Search placeholder={"Search a album of your choice"}></Search>
-  <Button> Give Feedback </Button>
-  
-  
-
+    <> 
+  <Navbar />
     </>
   )
 }
---
+++ my-react-app/src/components/nav/navbar.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import Button from '../Button/Button'
+import Search from '../Search/Search';
+import styles from './navbar.module.css'
+import logo from '../../assets/Q-tify.svg'
+
+
+const Navbar = () => {
+  return (
+  
+    <nav className={styles.navbar}>
+    <img clas src={logo} alt="Searcsh" />
+   <Search placeholder={"Search a album of your choice"}></Search>
+   <Button className={styles.feedback}> Give Feedback </Button>
+      
+
+    </nav>
+  );
+};
+
+export default Navbar;
