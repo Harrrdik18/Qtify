1e274aa45da8abc02e659d15ee805fb9264f3c17
hardik <harrrdik18@gmail.com>
2024-02-15 14:04:02 +0530

    added black

--- my-react-app/src/components/Hero/Hero.jsx
+++ my-react-app/src/components/Hero/Hero.jsx
@@ -1,14 +1,18 @@
+// Hero component
 import React from 'react';
-import Heros from '../../assets/Hero.png'
-import styles from './Hero.module.css'
-
+import Heros from '../../assets/Hero.png';
+import styles from './Hero.module.css';
 
 const Hero = () => {
-    return <div>
-        <img className={styles.hero} src={Heros} alt="hero img"/>
-
-    </div>;
-}
-
+    return (
+        <div className={styles.hero}>
+            <div>
+                <h1>100 Thousand Songs, ad-free</h1> 
+                <h1>Over thousands podcast episodes</h1>
+            </div>
+            <img className={styles.heroImage} src={Heros} alt="hero img"/>
+        </div>
+    );
+};
 
 export default Hero;
