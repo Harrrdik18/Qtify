0eefc5d74f97ced8fdb5f4e0397b7a14b034498d
hardik <harrrdik18@gmail.com>
2024-02-18 12:00:11 +0530

    added title

--- my-react-app/src/components/card/card.jsx
+++ my-react-app/src/components/card/card.jsx
@@ -8,10 +8,12 @@ const Card = ({ title, cover, follows }) => {
                 <div className={styles.inner}>
                     <img className={styles.cardimg} src={cover} alt={`Album cover for ${title}`} />
                     <div className={styles.content}>
-                        <h3 className={styles.title}>{title}</h3>
                         <h3 className={styles.followers}>{follows} followers</h3>
                     </div>
                 </div>
+            </div>
+            <div className={styles.titleContainer}>
+                <h3 className={styles.title}>{title}</h3>
             </div>
         </div>
     );
